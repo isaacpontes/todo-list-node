bab426ec4c87d0f6e5198913557a187ee88885a6
Isaac Pontes <isaaclfp@outlook.com>
2021-02-09 23:19:08 -0300

    Connected route and model to work with MongoDB.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const checklistRouter = require('./src/routes/checklist');
+require('./config/database');
 
 const app = express();
 app.use(express.json());
--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,20 +1,52 @@
 const express = require('express');
-
+const Checklist = require('../models/checklist');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    console.log('Checklist route accessed.');
-    res.send();
+router.get('/', async (req, res) => {
+    try {
+        const checklists = await Checklist.find({});
+        res.status(200).json(checklists);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 })
 
-router.post('/', (req, res) => {
-    console.log(req.body);
-    res.status(200).send(req.body);
+router.post('/', async (req, res) => {
+    const { name } = req.body;
+    try {
+        const checklist = await Checklist.create({ name });
+        res.status(200).json(checklist);
+    } catch (error) {
+        res.status(422).json(error);
+    }
 })
 
-router.get('/:id', (req, res) => {
-    console.log(req.params.id);
-    res.send({ "id": req.params.id });
+router.get('/:id', async (req, res) => {
+    try {
+        const checklist = await Checklist.findById(req.params.id);
+        res.status(200).json(checklist);
+    } catch (error) {
+        res.status(422).json(error);
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    const { name } = req.body;
+    try {
+        const checklist = await Checklist.findByIdAndUpdate(req.params.id, { name }, { new: true });
+        res.status(200).json(checklist);
+    } catch (error) {
+        res.status(422).json(error);
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const checklist = await Checklist.findByIdAndRemove(req.params.id);
+        res.status(200).json(checklist);
+    } catch (error) {
+        res.status(422).json(error);
+    }
 })
 
 module.exports = router;
