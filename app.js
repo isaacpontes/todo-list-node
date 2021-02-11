ba9ad0ce6844ecfff9574190e513c98ef9012024
Isaac Pontes <isaaclfp@outlook.com>
2021-02-10 21:59:29 -0300

    Created checklists CRUD with ejs.

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const methodOverride = require('method-override');
+const path = require('path');
+const indexRouter = require('./src/routes/index');
 const checklistRouter = require('./src/routes/checklist');
+
 require('./config/database');
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method', { methods: ['POST', 'GET'] }));
 
-app.use('/checklist', checklistRouter);
+app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
+app.use('/checklists', checklistRouter);
 
 app.listen(3000, () => {
     console.log('Server started.');
--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -5,48 +5,69 @@ const router = express.Router();
 router.get('/', async (req, res) => {
     try {
         const checklists = await Checklist.find({});
-        res.status(200).json(checklists);
+        res.status(200).render('checklists/index', { checklists: checklists });
     } catch (error) {
-        res.status(500).json(error);
+        res.status(500).render('pages/error', { error: 'Error getting checklists.' });
     }
-})
+});
+
+router.get('/new', async (req, res) => {
+    try {
+        const checklist = new Checklist();
+        res.status(200).render('checklists/new', { checklist: checklist });
+    } catch (error) {
+        res.status(500).render('pages/error', { error: 'Error loading form.' });
+    }
+});
+
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const checklist = await Checklist.findById(req.params.id);
+        res.status(200).render('checklists/edit', { checklist: checklist });
+    } catch (error) {
+        res.status(500).render('pages/error', { error: 'Error loading form.' });
+    }
+});
 
 router.post('/', async (req, res) => {
-    const { name } = req.body;
+    const { name } = req.body.checklist;
+    const checklist = new Checklist({ name });
     try {
-        const checklist = await Checklist.create({ name });
-        res.status(200).json(checklist);
+        checklist.save();
+        res.redirect('/checklists');
     } catch (error) {
-        res.status(422).json(error);
+        res.status(422).render('/checklists/new', { checklist: { ...checklist, error } });
     }
-})
+});
 
 router.get('/:id', async (req, res) => {
     try {
         const checklist = await Checklist.findById(req.params.id);
-        res.status(200).json(checklist);
+        res.status(200).render('checklists/show', { checklist: checklist });
     } catch (error) {
-        res.status(422).json(error);
+        res.status(500).render('pages/error', { error: 'Error getting checklist.' });
     }
-})
+});
 
 router.put('/:id', async (req, res) => {
-    const { name } = req.body;
+    const { name } = req.body.checklist;
+    const checklist = await Checklist.findById(req.params.id);
     try {
-        const checklist = await Checklist.findByIdAndUpdate(req.params.id, { name }, { new: true });
-        res.status(200).json(checklist);
+        await checklist.update({ name });
+        res.redirect('/checklists');
     } catch (error) {
-        res.status(422).json(error);
+        const errors = error.errors;
+        res.status(422).render('/checklists/edit', { checklist: { ...checklist, error } });
     }
-})
+});
 
 router.delete('/:id', async (req, res) => {
     try {
         const checklist = await Checklist.findByIdAndRemove(req.params.id);
-        res.status(200).json(checklist);
+        res.redirect('/checklists');
     } catch (error) {
-        res.status(422).json(error);
+        res.status(500).render('pages/error', { error: 'Error deleting checklist.' });
     }
-})
+});
 
 module.exports = router;
